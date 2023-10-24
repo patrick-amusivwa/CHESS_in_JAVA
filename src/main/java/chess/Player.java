cd0b6a19bc8d04f20b0c596ca19d2478469fcb67
Patrick amusivwa <patrick.amusivwa@students.jkuat.ac.ke>
2023-10-25 00:40:49 +0300

    update local files

---
+++ src/main/java/chess/Player.java
@@ -0,0 +1,20 @@
+package chess;
+
+public class Player {
+
+    public static String WHITE = "white";
+    public static String BLACK = "black";
+    private String color;//white or black
+
+    public Player(String color){
+
+        this.color = color;
+
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+
+}
